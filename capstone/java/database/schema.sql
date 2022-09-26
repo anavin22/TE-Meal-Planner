1d6d84f13c7564a7d28614fa7d5b1fb58bc06b3b
Erik Haggard <emhaggard8@gmail.com>
2022-09-26 18:15:57 -0400

    Added created_by in db

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -19,8 +19,10 @@ CREATE TABLE ingredient (
 
 CREATE TABLE recipe (
     recipe_id SERIAL,
+    created_by integer,
     recipe_name varchar(100) NOT NULL,
-    CONSTRAINT PK_recipe PRIMARY KEY (recipe_id)
+    CONSTRAINT PK_recipe PRIMARY KEY (recipe_id),
+    CONSTRAINT FK_created_by FOREIGN KEY (created_by) REFERENCES users(user_id)
 );
 
 CREATE TABLE unit (
