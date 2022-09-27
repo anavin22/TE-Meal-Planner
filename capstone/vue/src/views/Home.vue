6adb9f6f0d0070dfe2810814812eb845687534ee
Pavel Gaydos <pavel.gaydos@gmail.com>
2022-09-27 16:02:23 -0400

    Home.vue structure

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <h1>Browse Recipes</h1>
+    <!-- <p>You must be authenticated to see this</p> -->
+    <button>Add New Recipe</button>
+  
+
+    <div class="recipes-browse">
+      <div class="card" v-for="recipe in $store.state.recipes" :key="">
+        <h2>{{recipe.title}}</h2>
+        <p>{{recipe.description}}</p>
+        <!-- <router-link :to -->
+          <button>View Recipe</button>
+          <button>Save Recipe</button>
+        <!-- </router-link> -->
+      </div>
+    </div>
   </div>
 </template>
 
+
+
 <script>
 export default {
-  name: "home"
+  name: "home",
+
+
 };
 </script>
+
