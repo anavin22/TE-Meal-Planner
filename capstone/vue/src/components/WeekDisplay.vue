40347472abfb797e77d7f91234ff8b8ee2777131
Pavel Gaydos <pavel.gaydos@gmail.com>
2022-10-03 02:13:24 -0400

    MealPlanService + AddToMealPlanForm + added btn to SavedCard + Week Display

---
+++ capstone/vue/src/components/WeekDisplay.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="week-display">
+      <my-saved-recipes/>
+      <add-to-meal-plan-form/>
+  </div>
+</template>
+
+<script>
+import AddToMealPlanForm from './AddToMealPlanForm.vue'
+import MySavedRecipes from './MySavedRecipes.vue'
+export default {
+    name: "WeekDisplay",
+    components: {MySavedRecipes, AddToMealPlanForm},
+    props: [],
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
